640063e1acda1be812851fa6d573ee259ca9a097
shubhamkolhe18 <shubhkolhe18@gmail.com>
2025-01-31 16:09:52 +0530

    New components

--- force-app/main/default/lwc/practiceCompo/practiceCompo.js
+++ force-app/main/default/lwc/practiceCompo/practiceCompo.js
@@ -1,13 +1,23 @@
-import { LightningElement } from 'lwc';
+import { LightningElement, track } from 'lwc';
+import getContact from '@salesforce/apex/ContactController.getContact';
 
 export default class PracticeCompo extends LightningElement {
-
+    @track data = [];
     columns = [
-    { label: 'Name', fieldName: 'name' },
-    { label: 'Title', fieldName: 'Title' },
-    { label: 'Phone', fieldName: 'phone', type: 'phone' },
-    { label: 'Lead Source', fieldName: 'LeadSource' }
-];
-    columns ;
+        { label: 'Name', fieldName: 'Name' }, 
+        { label: 'Title', fieldName: 'Title' },
+        { label: 'Phone', fieldName: 'Phone', type: 'phone' }, 
+        { label: 'Lead Source', fieldName: 'LeadSource' } 
+    ];
 
+    connectedCallback() {
+        getContact()
+            .then(result => {
+                console.log('Data:', result);
+                this.data = result;
+            })
+            .catch(error => {
+                console.error('Error:', error);
+            });
+    }
 }
